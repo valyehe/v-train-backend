8859e0193308c7184fcc719f95e4b00e59c7f4a8
Admin <1052083012@qq.com>
2023-08-09 21:33:15 +0800

    修改启动日志，banner

--- train-member/src/main/java/com/v/MemberApplication.java
+++ train-member/src/main/java/com/v/MemberApplication.java
@@ -1,14 +1,21 @@
 package com.v;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class MemberApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MemberApplication.class, args);
-	}
+    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(MemberApplication.class);
+        Environment environment = application.run(args).getEnvironment();
+        LOG.info("成功启动端口："+environment.getProperty("server.port"));
+    }
 
 }
